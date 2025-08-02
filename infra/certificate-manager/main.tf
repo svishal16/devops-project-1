67009897a81c50581abdbf57cc7ac794d800f701
svishal16 <shrivastavavishal640@gmail.com>
2025-08-02 11:46:47 +0530

    update6 cert valid res commented

--- infra/certificate-manager/main.tf
+++ infra/certificate-manager/main.tf
@@ -39,7 +39,7 @@ resource "aws_route53_record" "validation" {
   ttl     = 60
 }
 
-resource "aws_acm_certificate_validation" "dev_proj_1_acm_validation" {
-  certificate_arn         = aws_acm_certificate.dev_proj_1_acm_arn.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
-}
+# resource "aws_acm_certificate_validation" "dev_proj_1_acm_validation" {
+#   certificate_arn         = aws_acm_certificate.dev_proj_1_acm_arn.arn
+#   validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
+# }
