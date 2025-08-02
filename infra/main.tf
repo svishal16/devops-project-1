dfa9c7d9be70aca1b6fdd0654528e1279879264c
svishal16 <shrivastavavishal640@gmail.com>
2025-08-02 14:00:50 +0530

    app run

--- infra/main.tf
+++ infra/main.tf
@@ -55,7 +55,7 @@ module "alb" {
   tag_name                  = "dev-proj-1-alb"
   lb_target_group_arn       = module.lb_target_group.dev_proj_1_lb_target_group_arn
   ec2_instance_id           = module.ec2.dev_proj_1_ec2_instance_id
-  lb_listner_port           = 5000
+  lb_listner_port           = 80
   lb_listner_protocol       = "HTTP"
   lb_listner_default_action = "forward"
   lb_https_listner_port     = 443
--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -4,12 +4,15 @@ set -e
 cd /home/ubuntu
 
 sudo apt update -y
-sudo apt install -y python3 python3-pip git
+sudo apt install -y python3 python3-pip python3.12-venv git
 git clone https://github.com/svishal16/python-mysql-db-proj-1.git
 sleep 20
 # shellcheck disable=SC2164
 cd python-mysql-db-proj-1
-pip3 install -r requirements.txt --break-system-packages --force-reinstall
+python3 -m venv venv
+source venv/bin/activate
+pip install flask pymysql
+pip install -r requirements.txt 
 echo 'Waiting for 30 seconds before running the app.py'
 
 export DB_HOST="${rds_endpoint}"
